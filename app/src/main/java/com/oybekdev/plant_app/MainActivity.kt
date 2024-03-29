52d9f82c4f63ede939d5c417c588f5bb8c269504
khurshiddev <umaribnxattob8008@gmail.com>
2024-03-29 17:54:43 +0900

    feat:add MainActivity.kt

--- app/src/main/java/com/oybekdev/plant_app/MainActivity.kt
+++ app/src/main/java/com/oybekdev/plant_app/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.oybekdev.plant_app.presentation.sign_up.SignUpScreen
 import com.oybekdev.plant_app.ui.theme.PlantappTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,14 +21,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PlantappTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    
-                }
+            NavigationView()
             }
         }
+    }
+}
+@Composable
+fun NavigationView(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "signup" ){
+        composable("signup"){ SignUpScreen(navController)}
     }
 }
